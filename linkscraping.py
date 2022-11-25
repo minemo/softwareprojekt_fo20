f5216aee690d0efc72dd35269f0913602a79207b
Max Stenzel <max.stenzel23@gmail.com>
2022-11-25 13:04:28 +0100

    continued get_important_words

--- linkscraping.py
+++ linkscraping.py
@@ -3,11 +3,25 @@ import requests
 import nltk
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize 
 
 def get_important_words(content: str):
-    html = urlopen(content).read()
+    html = urlopen(content).read()                  #get website text
     soup = BeautifulSoup(html, 'html.parser')
-    print(soup.text)
+    soup_string = soup.text
+    
+    stop_words = set(stopwords.words('german'))
+    
+    total_words = soup_string.split()               #get number of words
+    total_word_length = len(total_words)
+    print(total_word_length)
+
+    total_sentences = nltk.tokenize.sent_tokenize(soup_string)      #get number of sentences
+    total_sent_len = len(total_sentences)
+    print(total_sent_len)
+
+    #TODO 
 
 
 def get_image_content(link: str):
@@ -23,3 +37,4 @@ def extract_content(tweetid: int | str):
     else:
         return get_image_content(content.media[0].fullUrl)
 
+get_important_words("https://www.bpb.de/kurz-knapp/zahlen-und-fakten/globalisierung/52727/jaehrliche-aenderung-der-waldbestaende")
